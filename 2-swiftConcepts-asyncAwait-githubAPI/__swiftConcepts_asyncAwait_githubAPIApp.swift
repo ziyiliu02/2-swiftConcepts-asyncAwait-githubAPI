3106f55d84d935350ea9d492a0a9730e3ecf04b2
Liu Ziyi <ziyi.liu02@gmail.com>
2023-08-11 08:55:23 +0800

    Initial Commit

---
+++ 2-swiftConcepts-asyncAwait-githubAPI/__swiftConcepts_asyncAwait_githubAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  __swiftConcepts_asyncAwait_githubAPIApp.swift
+//  2-swiftConcepts-asyncAwait-githubAPI
+//
+//  Created by Liu Ziyi on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct __swiftConcepts_asyncAwait_githubAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
